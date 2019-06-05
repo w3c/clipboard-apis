fda57bbebdddb41c5e06fb90a3fb7152936ee945
Marcos Cáceres <mcaceres@mozilla.com>
2019-06-05 23:30:51 +1000

    chore(deploy): auto-publish

--- deploy.sh
+++ deploy.sh
@@ -64,3 +64,4 @@ ssh-add ../deploy_key
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
+bikeshed echidna --md-status=WD --u $USER --p $PASS --decision $DECISION --cc=marcos@marcosc.com
